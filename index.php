406cadeee35a0e91c2884a4ccdc9c4bf662d02d4
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-19 20:20:16 +0100

    created array matches

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$matches = [[
+    'home_team' => 'Virtus Bologna',
+    'away_team' => 'Dinamo Sassari',
+    'home_points' => rand(40,100),
+    'away_points' => rand(40,100),
+],
+[
+    'home_team' => 'Olimpia Milano',
+    'away_team' => 'V.L. Pesaro',
+    'home_points' => rand(40,100),
+    'away_points' => rand(40,100),
+],
+[
+    'home_team' => 'Reyer Venezia',
+    'away_team' => 'Fortitudo Bologna',
+    'home_points' => rand(40,100),
+    'away_points' => rand(40,100),
+],
+[
+    'home_team' => 'Pallacanestro Cantù',
+    'away_team' => 'Virtus Roma',
+    'home_points' => rand(40,100),
+    'away_points' => rand(40,100),
+],
+];
+
+var_dump($matches);
+?>
